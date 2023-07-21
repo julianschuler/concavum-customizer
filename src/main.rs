11a19eda9329c5c1b22a1146dec8033fd9461272
Julian Schuler <julianschuler@users.noreply.github.com>
2023-07-21 11:42:50 +0200

    Implement file watcher and event loop

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,60 @@
-fn main() {}
+mod window;
+
+use std::path::Path;
+
+use notify::{
+    event::{AccessKind::Close, AccessMode::Write},
+    recommended_watcher, Event,
+    EventKind::Access,
+    RecommendedWatcher, RecursiveMode, Watcher,
+};
+use window::Window;
+use winit::event_loop::EventLoopProxy;
+
+fn reload_model_on_config_change(
+    config_path: &Path,
+    event_loop_proxy: EventLoopProxy<()>,
+) -> notify::Result<RecommendedWatcher> {
+    let config_path_parent = config_path.parent().unwrap().to_path_buf();
+    let config_path = config_path.canonicalize().unwrap();
+
+    let mut watcher = recommended_watcher(move |result: notify::Result<Event>| {
+        if let Ok(event) = result {
+            if matches!(event.kind, Access(Close(Write))) {
+                if event.paths.iter().any(|path| path == &config_path) {
+                    let _ = event_loop_proxy.send_event(());
+                }
+            }
+        }
+    })?;
+
+    watcher.watch(&config_path_parent, RecursiveMode::NonRecursive)?;
+
+    Ok(watcher)
+}
+
+fn main() -> Result<(), Error> {
+    let config_path = Path::new("config.toml");
+
+    let window = Window::new()?;
+    let event_loop_proxy = window.event_loop_proxy();
+
+    let mut watcher = reload_model_on_config_change(config_path, event_loop_proxy)?;
+
+    window.run_event_loop()?;
+
+    watcher.unwatch(config_path)?;
+
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// Error initializing window
+    #[error("Error initializing main loop")]
+    WindowInit(#[from] window::Error),
+
+    /// Error initializing graphics
+    #[error("Error initializing graphics")]
+    Notify(#[from] notify::Error),
+}
