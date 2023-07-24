c67cc7bfd8e5c8e88acf4332b512f401ac8f3e23
Julian Schuler <julianschuler@users.noreply.github.com>
2023-07-25 01:08:37 +0200

    Port keyboard config and add example config

--- src/model/config.rs
+++ src/model/config.rs
@@ -1,6 +1,6 @@
 use std::{fs::read_to_string, io, path::Path};
 
-use glam::DVec3;
+use glam::{DVec2, DVec3};
 use hex_color::HexColor;
 use serde::Deserialize;
 
@@ -8,8 +8,48 @@ use serde::Deserialize;
 pub struct Config {
     pub points1: Vec<DVec3>,
     pub points2: Vec<DVec3>,
-    pub triangulation_tolerance: Option<f64>,
+
+    pub preview: Preview,
+    pub finger_cluster: FingerCluster,
+    pub thumb_cluster: ThumbCluster,
+    pub keyboard: Keyboard,
     pub colors: Colors,
+}
+
+#[derive(Deserialize)]
+pub struct Preview {
+    pub show_keys: bool,
+    pub show_interface_pcb: bool,
+    pub show_bottom_plate: bool,
+    pub triangulation_tolerance: Option<f64>,
+}
+
+#[derive(Deserialize)]
+pub struct FingerCluster {
+    pub rows: u8,
+    pub columns: Vec<Column>,
+    pub side_angles: (f64, f64),
+}
+
+#[derive(Deserialize)]
+pub struct Column {
+    pub curvature_angle: f64,
+    pub offset: DVec2,
+}
+
+#[derive(Deserialize)]
+pub struct ThumbCluster {
+    pub keys: u8,
+    pub curvature: f64,
+    pub rotation: DVec3,
+    pub offset: DVec3,
+}
+
+#[derive(Deserialize)]
+pub struct Keyboard {
+    pub tilting_angle: DVec2,
+    pub shell_thickness: f64,
+    pub bottom_plate_thickness: f64,
 }
 
 #[derive(Deserialize)]
