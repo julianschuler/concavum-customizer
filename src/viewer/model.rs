1703792d02dd6971d35cd9d801d47829a2be9cc3
Julian Schuler <julianschuler@users.noreply.github.com>
2023-07-25 15:11:25 +0200

    Allow placing component in multiple positions

--- src/viewer/model.rs
+++ src/viewer/model.rs
@@ -1,8 +1,11 @@
+use std::ops::Mul;
+
 use fj_interop::{
-    mesh::{Color, Mesh},
+    mesh::{Color, Mesh as FjMesh},
     model::Model,
 };
-use fj_math::{Aabb, Point, Triangle, Vector};
+use fj_math::{Aabb, Point, Triangle as FjTriangle};
+use glam::{DQuat, DVec3};
 use hex_color::HexColor;
 use opencascade::primitives::Shape;
 use opencascade_sys::ffi::{
@@ -11,17 +14,85 @@ use opencascade_sys::ffi::{
     TopLoc_Location_ctor, TopoDS_cast_to_face,
 };
 
+pub struct Triangle {
+    points: [DVec3; 3],
+}
+
+impl Triangle {
+    pub fn new(points: [DVec3; 3]) -> Self {
+        Self { points }
+    }
+
+    pub fn try_into_triangle(self) -> Option<FjTriangle<3>> {
+        let points = self
+            .points
+            .map(|point| Point::from_array([point.x, point.z, point.y]));
+
+        FjTriangle::from_points(points).ok()
+    }
+}
+
+#[derive(Clone)]
+pub struct Isometry {
+    rotation: DQuat,
+    translation: DVec3,
+}
+
+impl Isometry {
+    pub fn from_rotation_translation(rotation: DQuat, translation: DVec3) -> Self {
+        Self {
+            rotation,
+            translation,
+        }
+    }
+
+    pub const IDENTITY: Isometry = Isometry {
+        rotation: DQuat::IDENTITY,
+        translation: DVec3::ZERO,
+    };
+}
+
+impl Mul<DVec3> for &Isometry {
+    type Output = DVec3;
+
+    fn mul(self, vec: DVec3) -> Self::Output {
+        self.rotation * vec + self.translation
+    }
+}
+
+impl Mul<&Triangle> for &Isometry {
+    type Output = Triangle;
+
+    fn mul(self, triangle: &Triangle) -> Self::Output {
+        Triangle::new(triangle.points.map(|point| self * point))
+    }
+}
+
+pub struct Mesh {
+    triangles: Vec<Triangle>,
+    positions: Option<Vec<Isometry>>,
+}
+
 pub struct Component {
     shape: Shape,
     color: HexColor,
+    positions: Option<Vec<Isometry>>,
 }
 
 impl Component {
     pub fn new(shape: Shape, color: HexColor) -> Self {
-        Self { shape, color }
+        Self {
+            shape,
+            color,
+            positions: None,
+        }
     }
 
-    fn try_into_triangles(self, deflection_tolerance: f64) -> Result<Vec<Triangle<3>>, Error> {
+    pub fn with_positions(&mut self, positions: Vec<Isometry>) {
+        self.positions = Some(positions);
+    }
+
+    fn try_into_mesh(self, deflection_tolerance: f64) -> Result<Mesh, Error> {
         let mut triangles = Vec::new();
 
         let mut triangulation =
@@ -42,7 +113,7 @@ impl Component {
             let triangulation_handle = BRep_Tool_Triangulation(face, location.pin_mut());
             if let Ok(triangulation) = Handle_Poly_Triangulation_Get(&triangulation_handle) {
                 for index in 0..triangulation.NbTriangles() {
-                    let mut triangle_points = [Point::default(); 3];
+                    let mut triangle_points = [DVec3::default(); 3];
 
                     let triangle = triangulation.Triangle(index + 1);
 
@@ -52,27 +123,26 @@ impl Component {
                             triangle.Value(corner_index as i32 + 1),
                         );
                         let point = point.pin_mut();
+                        let point = DVec3::from_array([point.X(), point.Y(), point.Z()]);
 
-                        let coords = Vector {
-                            components: [point.X().into(), point.Z().into(), point.Y().into()],
-                        };
-                        triangle_points[corner_index] = Point { coords };
+                        triangle_points[corner_index] = point;
                     }
 
                     if face_orientation == TopAbs_Orientation::TopAbs_FORWARD {
                         triangle_points.reverse();
                     }
 
-                    if let Ok(triangle) = Triangle::from_points(triangle_points) {
-                        triangles.push(triangle);
-                    }
+                    triangles.push(Triangle::new(triangle_points));
                 }
             }
 
             face_explorer.pin_mut().Next();
         }
 
-        Ok(triangles)
+        Ok(Mesh {
+            triangles,
+            positions: self.positions,
+        })
     }
 }
 
@@ -81,20 +151,31 @@ pub trait ViewableModel {
 
     fn components(self) -> Vec<Component>;
 
-    fn into_mesh_model(self) -> fj_interop::model::Model
+    fn into_mesh_model(self) -> Model
     where
         Self: Sized,
     {
         let triangulation_tolerance = self.triangulation_tolerance();
-        let mut mesh = Mesh::new();
+        let mut mesh = FjMesh::new();
 
         for component in self.components() {
             let color = Color(component.color.to_be_bytes());
 
-            match component.try_into_triangles(triangulation_tolerance) {
-                Ok(triangles) => {
-                    for triangle in triangles {
-                        mesh.push_triangle(triangle, color);
+            match component.try_into_mesh(triangulation_tolerance) {
+                Ok(component_mesh) => {
+                    let Mesh {
+                        triangles,
+                        positions,
+                    } = component_mesh;
+
+                    let positions = positions.unwrap_or_else(|| vec![Isometry::IDENTITY]);
+
+                    for triangle in &triangles {
+                        for position in &positions {
+                            if let Some(triangle) = (position * triangle).try_into_triangle() {
+                                mesh.push_triangle(triangle, color);
+                            }
+                        }
                     }
                 }
                 Err(_) => {
