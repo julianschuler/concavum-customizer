c7e9d03e0b6c72471c6d5d34599eec5351444aca
Julian Schuler <julianschuler@users.noreply.github.com>
2023-12-15 09:11:12 +0100

    Update crates

--- src/viewer/model.rs
+++ src/viewer/model.rs
@@ -1,7 +1,7 @@
 use glam::{DAffine3, DMat4, DVec3};
 use hex_color::HexColor;
 use opencascade::{mesh::Mesh, primitives::Shape};
-use three_d::{Color, CpuMesh, Indices, Mat4, Positions, Vec3};
+use three_d::{CpuMesh, Indices, Mat4, Positions, Srgba, Vec3};
 
 pub struct Component {
     shape: Shape,
@@ -64,7 +64,7 @@ pub trait ViewableModel {
             .map(|position| position.as_vec3().to_array().into())
             .collect();
         let HexColor { r, g, b, a } = self.background_color();
-        let background_color = Color::new(r, g, b, a);
+        let background_color = Srgba::new(r, g, b, a);
 
         let mut objects = Vec::new();
 
@@ -72,7 +72,7 @@ pub trait ViewableModel {
             match component.mesh(triangulation_tolerance) {
                 Ok(mesh) => {
                     let HexColor { r, g, b, a } = component.color;
-                    let color = Color::new(r, g, b, a);
+                    let color = Srgba::new(r, g, b, a);
                     let transformations = component.positions.map(|positions| {
                         positions
                             .into_iter()
@@ -106,7 +106,7 @@ pub trait ViewableModel {
 #[derive(Clone)]
 pub struct CpuObject {
     pub mesh: CpuMesh,
-    pub color: Color,
+    pub color: Srgba,
     pub transformations: Option<Vec<Mat4>>,
 }
 
@@ -114,7 +114,7 @@ pub struct CpuObject {
 pub struct MeshModel {
     pub objects: Vec<CpuObject>,
     pub light_positions: Vec<Vec3>,
-    pub background_color: Color,
+    pub background_color: Srgba,
 }
 
 #[derive(Debug, thiserror::Error)]
