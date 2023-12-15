60d9f4b4755d7db9b8f5707eec20c5237bfae617
Julian Schuler <julianschuler@users.noreply.github.com>
2023-12-15 09:11:12 +0100

    Update crates

--- src/viewer/window.rs
+++ src/viewer/window.rs
@@ -2,42 +2,44 @@ use std::sync::Arc;
 
 use color_eyre::Report;
 use three_d::{
-    degrees, vec3, AmbientLight, Attenuation, Camera, ClearState, Color, Context, CpuMaterial,
-    Degrees, Event, FrameOutput, Gm, InnerSpace, InstancedMesh, Instances, Light, Mesh,
-    MouseButton, OrbitControl, PhysicalMaterial, PointLight, RenderTarget, Vec3, WindowError,
-    WindowSettings,
+    degrees, vec3, AmbientLight, Attenuation, Camera, ClearState, Context, CpuMaterial, Degrees,
+    FrameInputGenerator, Gm, InnerSpace, InstancedMesh, Instances, Light, Mesh, MouseButton,
+    OrbitControl, PhysicalMaterial, PointLight, RenderTarget, Srgba, SurfaceSettings, Vec3,
+    Viewport, WindowError, WindowedContext,
 };
-use winit::event_loop::{EventLoopBuilder, EventLoopProxy};
+use winit::{
+    event::Event,
+    event_loop::{ControlFlow, EventLoop, EventLoopBuilder, EventLoopProxy},
+    window::WindowBuilder,
+};
 
 use crate::{model::Error, viewer::model::MeshModel};
 
 pub type ModelUpdate = Result<MeshModel, Arc<Error>>;
 
 pub struct Window {
-    window: three_d::Window<ModelUpdate>,
-    event_loop_proxy: EventLoopProxy<ModelUpdate>,
+    event_loop: EventLoop<ModelUpdate>,
+    window: winit::window::Window,
+    context: WindowedContext,
 }
 
 impl Window {
     pub fn try_new() -> Result<Self, WindowError> {
         let event_loop = EventLoopBuilder::with_user_event().build();
-        let event_loop_proxy = event_loop.create_proxy();
-        let window = three_d::Window::from_event_loop(
-            WindowSettings {
-                title: "Concavum customizer".to_owned(),
-                ..Default::default()
-            },
-            event_loop,
-        )?;
+        let window = WindowBuilder::new()
+            .with_title("Concavum customizer")
+            .build(&event_loop)?;
+        let context = WindowedContext::from_winit_window(&window, SurfaceSettings::default())?;
 
         Ok(Self {
+            event_loop,
             window,
-            event_loop_proxy,
+            context,
         })
     }
 
     pub fn event_loop_proxy(&self) -> EventLoopProxy<ModelUpdate> {
-        self.event_loop_proxy.clone()
+        self.event_loop.create_proxy()
     }
 
     pub fn run_render_loop(self) {
@@ -45,10 +47,9 @@ impl Window {
         const DEFAULT_FOV: Degrees = degrees(22.5);
         const DEFAULT_TARGET: Vec3 = vec3(0.0, 0.0, 0.0);
 
-        let context = self.window.gl();
-
+        let (width, height): (u32, u32) = self.window.inner_size().into();
         let mut camera = Camera::new_perspective(
-            self.window.viewport(),
+            Viewport::new_at_origo(width, height),
             vec3(0.3, -1.0, 1.0).normalize_to(DEFAULT_DISTANCE),
             DEFAULT_TARGET, // camera target
             Vec3::unit_z(), // camera up
@@ -59,16 +60,49 @@ impl Window {
         let mut control = OrbitControl::new(DEFAULT_TARGET, 1.0, 1000.0);
         let mut scene = Scene::default();
 
-        self.window.render_loop(move |mut frame_input| {
-            camera.set_viewport(frame_input.viewport);
-            control.handle_events(&mut camera, &mut frame_input.events);
-            scene.handle_events(&mut camera, &mut frame_input.events, &context);
+        let mut frame_input_generator = FrameInputGenerator::from_winit_window(&self.window);
+        self.event_loop
+            .run(move |event, _, control_flow| match event {
+                Event::MainEventsCleared => {
+                    self.window.request_redraw();
+                }
+                Event::RedrawRequested(_) => {
+                    let mut frame_input = frame_input_generator.generate(&self.context);
 
-            let screen = frame_input.screen();
-            scene.render(&camera, &screen);
+                    control.handle_events(&mut camera, &mut frame_input.events);
+                    camera.set_viewport(frame_input.viewport);
+                    scene.handle_events(&mut camera, &mut frame_input.events);
 
-            FrameOutput::default()
-        })
+                    let screen = frame_input.screen();
+                    scene.render(&camera, &screen);
+
+                    self.context.swap_buffers().unwrap();
+                    *control_flow = ControlFlow::Poll;
+                    self.window.request_redraw();
+                }
+                Event::WindowEvent { ref event, .. } => {
+                    frame_input_generator.handle_winit_window_event(event);
+                    match event {
+                        winit::event::WindowEvent::Resized(physical_size) => {
+                            self.context.resize(*physical_size);
+                        }
+                        winit::event::WindowEvent::ScaleFactorChanged {
+                            new_inner_size, ..
+                        } => {
+                            self.context.resize(**new_inner_size);
+                        }
+                        winit::event::WindowEvent::CloseRequested => {
+                            control_flow.set_exit();
+                        }
+                        _ => (),
+                    }
+                }
+                Event::UserEvent(model_update) => match model_update {
+                    Ok(model) => scene = Scene::from_mesh_model(&self.context, &model),
+                    Err(err) => eprintln!("Error:{:?}", Report::from(err.to_owned())),
+                },
+                _ => {}
+            });
     }
 }
 
@@ -78,7 +112,7 @@ struct Scene {
     instanced_objects: Vec<Gm<InstancedMesh, PhysicalMaterial>>,
     lights: Vec<PointLight>,
     ambient: AmbientLight,
-    background_color: Color,
+    background_color: Srgba,
 }
 
 impl Scene {
@@ -109,7 +143,7 @@ impl Scene {
             }
         }
 
-        let ambient = AmbientLight::new(context, 0.05, Color::WHITE);
+        let ambient = AmbientLight::new(context, 0.05, Srgba::WHITE);
         let lights = model
             .light_positions
             .iter()
@@ -117,7 +151,7 @@ impl Scene {
                 PointLight::new(
                     context,
                     1.0,
-                    Color::WHITE,
+                    Srgba::WHITE,
                     direction,
                     Attenuation::default(),
                 )
@@ -133,15 +167,10 @@ impl Scene {
         }
     }
 
-    fn handle_events(
-        &mut self,
-        camera: &mut Camera,
-        events: &mut [Event<ModelUpdate>],
-        context: &Context,
-    ) {
+    fn handle_events(&mut self, camera: &mut Camera, events: &mut [three_d::Event]) {
         for event in events {
             match event {
-                Event::MouseMotion { button, delta, .. } => {
+                three_d::Event::MouseMotion { button, delta, .. } => {
                     if *button == Some(MouseButton::Right) {
                         let right = camera.right_direction().normalize();
                         let up = right.cross(camera.view_direction());
@@ -151,17 +180,13 @@ impl Scene {
                         camera.translate(&(speed * translation));
                     }
                 }
-                Event::UserEvent(model_update) => match model_update {
-                    Ok(model) => *self = Scene::from_mesh_model(context, model),
-                    Err(err) => eprintln!("Error:{:?}", Report::from(err.to_owned())),
-                },
                 _ => (),
             }
         }
     }
 
     fn render(&self, camera: &Camera, screen: &RenderTarget) {
-        let [r, g, b, a] = self.background_color.to_rgba_slice();
+        let Srgba { r, g, b, a } = self.background_color;
 
         let mut lights: Vec<_> = self
             .lights
@@ -170,8 +195,15 @@ impl Scene {
             .collect();
         lights.push(&self.ambient as &dyn Light);
 
-        screen.clear(ClearState::color_and_depth(r, g, b, a, 1.0));
-        screen.render(camera, &self.objects, &lights);
-        screen.render(camera, &self.instanced_objects, &lights);
+        screen
+            .clear(ClearState::color_and_depth(
+                r as f32 / 255.0,
+                g as f32 / 255.0,
+                b as f32 / 255.0,
+                a as f32 / 255.0,
+                1.0,
+            ))
+            .render(camera, &self.objects, &lights)
+            .render(camera, &self.instanced_objects, &lights);
     }
 }
