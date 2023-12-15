80289b30fcc3214754b1b4bb1d10cda46560974c
Julian Schuler <julianschuler@users.noreply.github.com>
2023-12-15 10:09:54 +0100

    Refactor event loop handling

--- src/viewer/window.rs
+++ src/viewer/window.rs
@@ -8,7 +8,7 @@ use three_d::{
     Viewport, WindowError, WindowedContext,
 };
 use winit::{
-    event::Event,
+    event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop, EventLoopBuilder, EventLoopProxy},
     window::WindowBuilder,
 };
@@ -43,66 +43,106 @@ impl Window {
     }
 
     pub fn run_render_loop(self) {
-        const DEFAULT_DISTANCE: f32 = 300.0;
-        const DEFAULT_FOV: Degrees = degrees(22.5);
-        const DEFAULT_TARGET: Vec3 = vec3(0.0, 0.0, 0.0);
-
+        let frame_input_generator = FrameInputGenerator::from_winit_window(&self.window);
         let (width, height): (u32, u32) = self.window.inner_size().into();
-        let mut camera = Camera::new_perspective(
-            Viewport::new_at_origo(width, height),
-            vec3(0.3, -1.0, 1.0).normalize_to(DEFAULT_DISTANCE),
-            DEFAULT_TARGET, // camera target
-            Vec3::unit_z(), // camera up
-            DEFAULT_FOV,
-            0.1,
-            10000.0,
-        );
-        let mut control = OrbitControl::new(DEFAULT_TARGET, 1.0, 1000.0);
-        let mut scene = Scene::default();
+        let viewport = Viewport::new_at_origo(width, height);
+        let mut application = Application::new(frame_input_generator, viewport);
 
-        let mut frame_input_generator = FrameInputGenerator::from_winit_window(&self.window);
         self.event_loop
             .run(move |event, _, control_flow| match event {
                 Event::MainEventsCleared => {
                     self.window.request_redraw();
                 }
                 Event::RedrawRequested(_) => {
-                    let mut frame_input = frame_input_generator.generate(&self.context);
-
-                    control.handle_events(&mut camera, &mut frame_input.events);
-                    camera.set_viewport(frame_input.viewport);
-                    scene.handle_events(&mut camera, &mut frame_input.events);
-
-                    let screen = frame_input.screen();
-                    scene.render(&camera, &screen);
+                    application.handle_redraw(&self.context);
 
                     self.context.swap_buffers().unwrap();
-                    *control_flow = ControlFlow::Poll;
+                    control_flow.set_poll();
                     self.window.request_redraw();
                 }
                 Event::WindowEvent { ref event, .. } => {
-                    frame_input_generator.handle_winit_window_event(event);
-                    match event {
-                        winit::event::WindowEvent::Resized(physical_size) => {
-                            self.context.resize(*physical_size);
-                        }
-                        winit::event::WindowEvent::ScaleFactorChanged {
-                            new_inner_size, ..
-                        } => {
-                            self.context.resize(**new_inner_size);
-                        }
-                        winit::event::WindowEvent::CloseRequested => {
-                            control_flow.set_exit();
-                        }
-                        _ => (),
-                    }
+                    application.handle_window_event(event, &self.context, control_flow);
                 }
-                Event::UserEvent(model_update) => match model_update {
-                    Ok(model) => scene = Scene::from_mesh_model(&self.context, &model),
-                    Err(err) => eprintln!("Error:{:?}", Report::from(err.to_owned())),
-                },
+                Event::UserEvent(model_update) => {
+                    application.handle_model_update(model_update, &self.context);
+                }
                 _ => {}
             });
+    }
+}
+
+struct Application {
+    control: OrbitControl,
+    camera: Camera,
+    scene: Scene,
+    frame_input_generator: FrameInputGenerator,
+}
+
+impl Application {
+    fn new(frame_input_generator: FrameInputGenerator, viewport: Viewport) -> Self {
+        const DEFAULT_DISTANCE: f32 = 300.0;
+        const DEFAULT_FOV: Degrees = degrees(22.5);
+        const DEFAULT_TARGET: Vec3 = vec3(0.0, 0.0, 0.0);
+
+        let camera = Camera::new_perspective(
+            viewport,
+            vec3(0.3, -1.0, 1.0).normalize_to(DEFAULT_DISTANCE),
+            DEFAULT_TARGET,
+            Vec3::unit_z(),
+            DEFAULT_FOV,
+            0.1,
+            10000.0,
+        );
+        let control = OrbitControl::new(DEFAULT_TARGET, 1.0, 1000.0);
+        let scene = Scene::default();
+
+        Self {
+            control,
+            camera,
+            scene,
+            frame_input_generator,
+        }
+    }
+
+    fn handle_redraw(&mut self, context: &WindowedContext) {
+        let mut frame_input = self.frame_input_generator.generate(context);
+
+        self.control
+            .handle_events(&mut self.camera, &mut frame_input.events);
+        self.camera.set_viewport(frame_input.viewport);
+        self.scene
+            .handle_events(&mut self.camera, &mut frame_input.events);
+
+        let screen = frame_input.screen();
+        self.scene.render(&self.camera, &screen);
+    }
+
+    fn handle_window_event(
+        &mut self,
+        event: &WindowEvent,
+        context: &WindowedContext,
+        control_flow: &mut ControlFlow,
+    ) {
+        self.frame_input_generator.handle_winit_window_event(event);
+        match event {
+            WindowEvent::Resized(physical_size) => {
+                context.resize(*physical_size);
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                context.resize(**new_inner_size);
+            }
+            WindowEvent::CloseRequested => {
+                control_flow.set_exit();
+            }
+            _ => (),
+        }
+    }
+
+    fn handle_model_update(&mut self, model_update: ModelUpdate, context: &WindowedContext) {
+        match model_update {
+            Ok(model) => self.scene = Scene::from_mesh_model(&model, context),
+            Err(err) => eprintln!("Error:{:?}", Report::from(err.to_owned())),
+        }
     }
 }
 
@@ -116,7 +156,7 @@ struct Scene {
 }
 
 impl Scene {
-    fn from_mesh_model(context: &Context, model: &MeshModel) -> Scene {
+    fn from_mesh_model(model: &MeshModel, context: &Context) -> Scene {
         let mut objects = Vec::new();
         let mut instanced_objects = Vec::new();
 
@@ -169,18 +209,15 @@ impl Scene {
 
     fn handle_events(&mut self, camera: &mut Camera, events: &mut [three_d::Event]) {
         for event in events {
-            match event {
-                three_d::Event::MouseMotion { button, delta, .. } => {
-                    if *button == Some(MouseButton::Right) {
-                        let right = camera.right_direction().normalize();
-                        let up = right.cross(camera.view_direction());
-                        let translation = -delta.0 as f32 * right + delta.1 as f32 * up;
-                        let speed = 0.001 * camera.position().magnitude();
+            if let three_d::Event::MouseMotion { button, delta, .. } = event {
+                if *button == Some(MouseButton::Right) {
+                    let right = camera.right_direction().normalize();
+                    let up = right.cross(camera.view_direction());
+                    let translation = -delta.0 as f32 * right + delta.1 as f32 * up;
+                    let speed = 0.001 * camera.position().magnitude();
 
-                        camera.translate(&(speed * translation));
-                    }
+                    camera.translate(&(speed * translation));
                 }
-                _ => (),
             }
         }
     }
