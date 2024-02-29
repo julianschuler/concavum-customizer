d09996c5f43bb0d12dc97d52dc55d2bef5d445e2
Julian Schuler <julianschuler@users.noreply.github.com>
2024-02-29 19:09:35 +0100

    Add CSG operations

--- src/model/primitives.rs
+++ src/model/primitives.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use fidget::{
     context::{IntoNode, Node},
     Context, Error,
@@ -12,7 +14,6 @@ pub struct Sphere {
     radius: f64,
 }
 
-#[allow(unused)]
 impl Sphere {
     pub fn new(radius: f64) -> Self {
         Self { radius }
@@ -54,6 +55,67 @@ impl IntoNode for BoxShape {
         let inner = context.min(max_elem, 0.0)?;
 
         context.add(outer, inner)
+    }
+}
+
+pub trait Csg {
+    /// Extrude a 2D shape to a given height
+    fn extrusion<T>(&mut self, shape: T, height: f64) -> Result<Node>
+    where
+        T: IntoNode;
+
+    /// Perform the union between two 3D shapes
+    fn union<A, B>(&mut self, a: A, b: B) -> Result<Node>
+    where
+        A: IntoNode,
+        B: IntoNode;
+
+    /// Perform the difference between two 3D shapes
+    fn difference<A, B>(&mut self, a: A, b: B) -> Result<Node>
+    where
+        A: IntoNode,
+        B: IntoNode;
+
+    /// Perform the intersection between two 3D shapes
+    fn intersection<A, B>(&mut self, a: A, b: B) -> Result<Node>
+    where
+        A: IntoNode,
+        B: IntoNode;
+}
+
+impl Csg for Context {
+    fn extrusion<T: IntoNode>(&mut self, node: T, height: f64) -> Result<Node> {
+        let z = self.z();
+        let neg_z = self.neg(z)?;
+        let diff = self.sub(z, height)?;
+        let dist_z = self.max(neg_z, diff)?;
+
+        self.max(node, dist_z)
+    }
+
+    fn union<A, B>(&mut self, a: A, b: B) -> Result<Node>
+    where
+        A: IntoNode,
+        B: IntoNode,
+    {
+        self.min(a, b)
+    }
+
+    fn difference<A, B>(&mut self, a: A, b: B) -> Result<Node>
+    where
+        A: IntoNode,
+        B: IntoNode,
+    {
+        let neg_a = self.neg(a)?;
+        self.max(neg_a, b)
+    }
+
+    fn intersection<A, B>(&mut self, a: A, b: B) -> Result<Node>
+    where
+        A: IntoNode,
+        B: IntoNode,
+    {
+        self.max(a, b)
     }
 }
 
