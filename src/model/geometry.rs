465004fcec9ae1e61150d99ff5cd47351d5b016b
Julian Schuler <julianschuler@users.noreply.github.com>
2024-03-06 20:39:21 +0100

    Inline Translate and Rotate trait functions

--- src/model/geometry.rs
+++ src/model/geometry.rs
@@ -1,37 +1,7 @@
-use glam::{dvec3, DAffine3, DVec2, DVec3};
+use glam::{dvec3, DVec2, DVec3};
 
 pub fn zvec(z: f64) -> DVec3 {
     dvec3(0.0, 0.0, z)
-}
-
-pub trait Translate {
-    fn translate(self, translation: DVec3) -> Self;
-}
-
-impl Translate for DAffine3 {
-    fn translate(self, translation: DVec3) -> Self {
-        DAffine3::from_translation(translation) * self
-    }
-}
-
-pub trait Rotate {
-    fn rotate_x(self, angle: f64) -> Self;
-    fn rotate_y(self, angle: f64) -> Self;
-    fn rotate_z(self, angle: f64) -> Self;
-}
-
-impl Rotate for DAffine3 {
-    fn rotate_x(self, angle: f64) -> Self {
-        DAffine3::from_rotation_x(angle) * self
-    }
-
-    fn rotate_y(self, angle: f64) -> Self {
-        DAffine3::from_rotation_y(angle) * self
-    }
-
-    fn rotate_z(self, angle: f64) -> Self {
-        DAffine3::from_rotation_z(angle) * self
-    }
 }
 
 pub struct Line {
