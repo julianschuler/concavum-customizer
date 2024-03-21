cd4d3c72d93428e45a817ea813488a417a2efa5e
Julian Schuler <julianschuler@users.noreply.github.com>
2024-03-21 16:56:53 +0100

    Add Transform operations

--- src/model/primitives/mod.rs
+++ src/model/primitives/mod.rs
@@ -1,10 +1,8 @@
-#![allow(unused)]
-
 mod operations;
 mod shapes;
 mod vector;
 
 type Result<T> = std::result::Result<T, fidget::Error>;
 
-pub use operations::Csg;
+pub use operations::*;
 pub use shapes::*;
