51d9d7cf57cb5931ec5d4131be0bed80bac29271
Julian Schuler <julianschuler@users.noreply.github.com>
2024-03-21 13:30:36 +0100

    Use convex partition for simple polygon SDF
    The prior implementation was not C0 continuous and thus resulted in
    undefined meshing behaviour.

--- src/model/primitives/shapes.rs
+++ src/model/primitives/shapes.rs
@@ -253,61 +253,52 @@ impl SimplePolygon {
 impl IntoNode for SimplePolygon {
     fn into_node(self, context: &mut Context) -> Result<Node> {
         let point = Vec2::point(context);
-        let length = context.vec_length(point)?;
+        let first = *self.vertices.first().unwrap();
+        let last = *self.vertices.last().unwrap();
 
-        let vertices: Vec<_> = self
+        let mut squared_abs = context.constant(INFINITY);
+
+        for window in self
             .vertices
-            .into_iter()
-            .map(|vertex| Vec2::from_parameter(context, vertex))
-            .collect();
-
-        let first = vertices.first().unwrap();
-        let last = vertices.last().unwrap();
-        let diff = context.vec_sub(point, *first)?;
-
-        let mut squared_distance = context.vec_dot(diff, diff)?;
-        let mut sign = context.constant(1.0);
-
-        for window in vertices.windows(2).chain(once([*last, *first].as_slice())) {
+            .windows(2)
+            .chain(once([last, first].as_slice()))
+        {
             let previous_vertex = window[0];
             let vertex = window[1];
 
-            let edge = context.vec_sub(previous_vertex, vertex)?;
+            let edge = previous_vertex - vertex;
+            let edge_length = edge.length();
+            let edge = edge.normalize_or_zero();
+            let edge = Vec2::from_parameter(context, edge);
+            let vertex = Vec2::from_parameter(context, vertex);
             let diff = context.vec_sub(point, vertex)?;
 
             // Calculate shortest possible vector from point to edge
             let edge_projection = context.vec_dot(diff, edge)?;
-            let edge_length_squared = context.vec_dot(edge, edge)?;
-            let closest_point_factor = context.div(edge_projection, edge_length_squared)?;
-
-            let max = context.max(closest_point_factor, 0.0)?;
-            let clamped_factor = context.min(max, 1.0)?;
+            let max = context.max(edge_projection, 0.0)?;
+            let clamped_factor = context.min(max, edge_length)?;
             let scaled_edge = context.vec_mul(clamped_factor, edge)?;
             let shortest_diff = context.vec_sub(diff, scaled_edge)?;
 
-            let shortest_distance = context.vec_dot(shortest_diff, shortest_diff)?;
-            squared_distance = context.min(squared_distance, shortest_distance)?;
-
-            // Calculate winding number (determine whether point is inside or outside polygon)
-            let point_above_vertex = context.less_than(vertex.y, point.y)?;
-            let point_below_previous_vertex = context.less_than(point.y, previous_vertex.y)?;
-            let m1 = context.mul(edge.y, diff.x)?;
-            let m2 = context.mul(edge.x, diff.y)?;
-            let m1_less_than_m2 = context.less_than(m1, m2)?;
-
-            // Multiply sign by -1.0 if all or none of the conditions are true
-            let sum = context.add(point_above_vertex, point_below_previous_vertex)?;
-            let sum = context.add(sum, m1_less_than_m2)?;
-            let shifted = context.sub(sum, 1.5)?;
-            let abs = context.abs(shifted)?;
-            let indicator = context.sub(1.5, abs)?;
-            let scaled = context.mul(2.0, indicator)?;
-            let indicator = context.sub(scaled, 1.0)?;
-            sign = context.mul(sign, indicator)?;
+            let shortest_abs = context.vec_dot(shortest_diff, shortest_diff)?;
+            squared_abs = context.min(squared_abs, shortest_abs)?;
         }
 
-        let distance = context.sqrt(squared_distance)?;
-        context.mul(sign, distance)
+        // Calculate outer distance from outer distances of convex partition
+        let mut outer_distance = context.constant(INFINITY);
+        for polygon in self.split_into_convex_polygons() {
+            let Distances { absolute, inner } = polygon.distances(context)?;
+            let outer = context.add(absolute, inner)?;
+
+            outer_distance = context.min(outer_distance, outer)?;
+        }
+        let double_outer = context.mul(2.0, outer_distance)?;
+
+        // Clamp to EPSILON to get well-behaved gradients
+        let max = context.max(squared_abs, EPSILON)?;
+        let absolute_distance = context.sqrt(max)?;
+
+        context.sub(double_outer, absolute_distance)
     }
 }
 
