70154c9e05a20510d1bf6f612e5e1dfb14525821
Julian Schuler <julianschuler@users.noreply.github.com>
2024-03-25 21:55:05 +0100

    Port switch and keycap to SDF representation

--- src/model/key.rs
+++ src/model/key.rs
@@ -1,13 +1,11 @@
-use glam::{dvec2, dvec3, DVec2};
+use fidget::Context;
+use glam::{dvec2, dvec3, DVec2, DVec3};
 use hex_color::HexColor;
-use opencascade::{
-    primitives::{Shape, Solid},
-    workplane::Workplane,
-};
 
 use crate::model::{
     config::{Config, EPSILON},
     geometry::zvec,
+    primitives::{BoundingBox, BoxShape, Csg, Rectangle, Result, Shape, Transforms},
     Component,
 };
 
@@ -17,11 +15,11 @@ pub struct Key {
 }
 
 impl Key {
-    pub fn new(config: &Config, size_u: f64) -> Key {
-        let switch = Switch::new(config);
-        let keycap = KeyCap::new(config, size_u);
+    pub fn new(config: &Config, size_u: f64) -> Result<Key> {
+        let switch = Switch::new(config)?;
+        let keycap = KeyCap::new(config, size_u)?;
 
-        Self { switch, keycap }
+        Ok(Self { switch, keycap })
     }
 }
 
@@ -38,47 +36,42 @@ pub struct KeyCap {
 }
 
 impl KeyCap {
-    pub fn new(config: &Config, size_u: f64) -> KeyCap {
+    pub fn new(config: &Config, size_u: f64) -> Result<KeyCap> {
         const KEYCAP_PITCH: f64 = 19.05;
         const BOTTOM_WIDTH: f64 = 18.2;
         const BOTTOM_FILLET: f64 = 0.5;
-        const MIDDLE_WIDTH: f64 = 15.8;
         const TOP_WIDTH: f64 = 12.0;
-        const TOP_FILLET: f64 = 2.0;
         const HEIGHT: f64 = 7.0;
 
         let bottom_length = KEYCAP_PITCH * size_u - (KEYCAP_PITCH - BOTTOM_WIDTH);
-        let mid_length = KEYCAP_PITCH * size_u - (KEYCAP_PITCH - MIDDLE_WIDTH);
         let top_length = KEYCAP_PITCH * size_u - (KEYCAP_PITCH - TOP_WIDTH);
+        let scale = dvec2(TOP_WIDTH / BOTTOM_WIDTH, top_length / bottom_length);
 
-        let bottom = Workplane::xy()
-            .rect(BOTTOM_WIDTH, bottom_length)
-            .fillet(BOTTOM_FILLET);
+        let mut context = Context::new();
 
-        let middle = Workplane::xy()
-            .translated(zvec(HEIGHT / 2.0))
-            .rect(MIDDLE_WIDTH, mid_length)
-            .fillet((TOP_FILLET + BOTTOM_FILLET) / 2.0);
+        let rectangle = Rectangle::new(dvec2(
+            BOTTOM_WIDTH - 2.0 * BOTTOM_FILLET,
+            bottom_length - 2.0 * BOTTOM_FILLET,
+        ));
+        let rounded_rectangle = context.offset(rectangle, BOTTOM_FILLET)?;
 
-        let top = Workplane::xy()
-            .translated(zvec(HEIGHT))
-            .rect(TOP_WIDTH, top_length)
-            .fillet(TOP_FILLET);
+        let shape = context.extrude(rounded_rectangle, HEIGHT)?;
+        let shape = context.taper(shape, scale, zvec(HEIGHT))?;
+        let root = context.translate(shape, zvec(Switch::TOP_HEIGHT))?;
 
-        let mut shape: Shape = Solid::loft([&bottom, &middle, &top]).into();
+        let bounding_box = BoundingBox::new(20.0, DVec3::ZERO);
+        let shape = Shape::new(&context, root, bounding_box)?;
 
-        shape.set_global_translation(zvec(Switch::TOP_HEIGHT));
-
-        Self {
-            color: config.colors.keycap,
+        Ok(Self {
             shape,
-        }
+            color: config.colors.keycap,
+        })
     }
 }
 
 impl From<KeyCap> for Component {
     fn from(keycap: KeyCap) -> Self {
-        Component::new(todo!(), keycap.color)
+        Component::new(keycap.shape, keycap.color)
     }
 }
 
@@ -90,35 +83,37 @@ pub struct Switch {
 impl Switch {
     pub const TOP_HEIGHT: f64 = 6.6;
 
-    pub fn new(config: &Config) -> Switch {
+    pub fn new(config: &Config) -> Result<Switch> {
         const BOTTOM_WIDTH: f64 = 14.0;
         const BOTTOM_HEIGHT: f64 = 5.5;
         const TOP_WIDTH: f64 = 15.6;
         const TOP_SIZE: DVec2 = dvec2(11.0, 10.0);
         const TOP_OFFSET: f64 = 1.5;
+        let scale = TOP_SIZE / dvec2(TOP_WIDTH, TOP_WIDTH);
 
-        let bottom_rect = Workplane::xy()
-            .translated(zvec(-BOTTOM_HEIGHT))
-            .rect(BOTTOM_WIDTH, BOTTOM_WIDTH);
-        let bottom = bottom_rect.to_face().extrude(zvec(BOTTOM_HEIGHT + EPSILON));
+        let mut context = Context::new();
 
-        let top_lower_rect = Workplane::xy().rect(TOP_WIDTH, TOP_WIDTH);
-        let top_upper_rect = Workplane::xy()
-            .translated(dvec3(0.0, TOP_OFFSET, Self::TOP_HEIGHT))
-            .rect(TOP_SIZE.x, TOP_SIZE.y);
+        let bottom = BoxShape::new(dvec3(BOTTOM_WIDTH, BOTTOM_WIDTH, BOTTOM_HEIGHT + EPSILON));
+        let bottom = context.translate(bottom, zvec(-(BOTTOM_HEIGHT + EPSILON) / 2.0))?;
 
-        let shape: Shape = Solid::loft([&top_lower_rect, &top_upper_rect]).into();
-        let shape = shape.union(&bottom.into()).into();
+        let top = BoxShape::new(dvec3(TOP_WIDTH, TOP_WIDTH, Self::TOP_HEIGHT));
+        let top = context.translate(top, zvec(Self::TOP_HEIGHT / 2.0))?;
+        let top = context.taper(top, scale, dvec3(0.0, TOP_OFFSET, Self::TOP_HEIGHT))?;
 
-        Self {
-            color: config.colors.switch,
+        let union = context.union(bottom, top)?;
+
+        let bounding_box = BoundingBox::new(16.0, DVec3::ZERO);
+        let shape = Shape::new(&context, union, bounding_box)?;
+
+        Ok(Self {
             shape,
-        }
+            color: config.colors.switch,
+        })
     }
 }
 
 impl From<Switch> for Component {
     fn from(switch: Switch) -> Self {
-        Component::new(todo!(), switch.color)
+        Component::new(switch.shape, switch.color)
     }
 }
