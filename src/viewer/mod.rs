5e62c53893ffbe69ffdcb6b5d4eac37b85cde75d
Julian Schuler <julianschuler@users.noreply.github.com>
2024-04-04 11:42:17 +0200

    Rename ModelReloader to FileWatcher

--- src/viewer/mod.rs
+++ src/viewer/mod.rs
@@ -1,8 +1,8 @@
+mod file_watcher;
 mod material;
 mod model;
-mod reload;
 mod window;
 
+pub use file_watcher::FileWatcher;
 pub use model::{Component, MeshSettings, Viewable};
-pub use reload::ModelReloader;
 pub use window::{ModelUpdater, ReloadEvent, Window};
