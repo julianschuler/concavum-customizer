9933af2f3c6f38425d586de6bd42d672c15adec2
Julian Schuler <julianschuler@users.noreply.github.com>
2024-04-24 12:04:23 +0200

    Move model reloading to separate thread

--- src/viewer/reload.rs
+++ src/viewer/reload.rs
@@ -1,4 +1,11 @@
-use std::{sync::Arc, time::Instant};
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread::spawn,
+    time::Instant,
+};
 
 use fidget::mesh::Settings;
 
@@ -13,39 +20,82 @@ use crate::{
 
 pub struct ModelReloader {
     updater: ModelUpdater,
+    cancellation_token: CancellationToken,
 }
 
 impl ModelReloader {
     /// Creates a new model reloader using the given updater for updating the model.
     pub fn new(updater: ModelUpdater) -> Self {
-        Self { updater }
+        Self {
+            updater,
+            cancellation_token: CancellationToken::new(),
+        }
     }
 
-    pub fn reload(&self, config: Result<Config, Error>) {
+    pub fn reload(&mut self, config: Result<Config, Error>) {
         match config {
             Ok(config) => {
-                let start = Instant::now();
+                self.cancellation_token.cancel();
+                let cancellation_token = CancellationToken::new();
+                self.cancellation_token = cancellation_token.clone();
 
+                let start = Instant::now();
                 self.updater.send_event(ReloadEvent::Started);
                 let model = Model::from_config(config);
 
                 let mesh_settings = model.mesh_settings();
-                for depth in 0..mesh_settings.depth {
-                    let mesh_settings = Settings {
-                        depth,
-                        bounds: mesh_settings.bounds,
-                        threads: mesh_settings.threads,
-                    };
-                    let model = model.into_model(mesh_settings);
-                    self.updater.send_event(ReloadEvent::Updated(model));
-                }
+                let cancellation_token = self.cancellation_token.clone();
+                let updater = self.updater.clone();
+                spawn(move || {
+                    let mut cancelled = false;
+                    for depth in 0..mesh_settings.depth {
+                        let mesh_settings = Settings {
+                            depth,
+                            bounds: mesh_settings.bounds,
+                            threads: mesh_settings.threads,
+                        };
+                        let model = model.into_model(mesh_settings);
 
-                let model = model.into_model(mesh_settings);
-                self.updater.send_event(ReloadEvent::Finished(model));
+                        cancelled = cancellation_token.cancelled();
+                        if cancelled {
+                            break;
+                        }
+                        updater.send_event(ReloadEvent::Updated(model));
+                    }
 
-                eprintln!("Reloaded model in {:?}", start.elapsed());
+                    if !cancelled {
+                        let model = model.into_model(mesh_settings);
+
+                        if !cancellation_token.cancelled() {
+                            updater.send_event(ReloadEvent::Finished(model));
+
+                            eprintln!("Reloaded model in {:?}", start.elapsed());
+                        }
+                    }
+                });
             }
             Err(error) => self.updater.send_event(ReloadEvent::Error(Arc::new(error))),
         }
+    }
+}
+
+#[derive(Clone)]
+struct CancellationToken {
+    cancelled: Arc<AtomicBool>,
+}
+
+impl CancellationToken {
+    fn new() -> Self {
+        Self {
+            cancelled: Arc::new(AtomicBool::new(false)),
+        }
+    }
+
+    fn cancel(&self) {
+        self.cancelled.store(true, Ordering::Release);
+    }
+
+    fn cancelled(&self) -> bool {
+        self.cancelled.load(Ordering::Acquire)
     }
 }
