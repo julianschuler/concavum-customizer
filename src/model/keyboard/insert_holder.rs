5d609722ccd4e0e7dd496946457b365e76672ca1
Julian Schuler <julianschuler@users.noreply.github.com>
2024-06-23 16:16:29 +0200

    Split insert holder creation and Tree calculation

--- src/model/keyboard/insert_holder.rs
+++ src/model/keyboard/insert_holder.rs
@@ -9,44 +9,53 @@ use crate::{
     },
 };
 
-pub struct InsertHolder(Tree);
+pub struct InsertHolder {
+    point: DVec2,
+    edge1: DVec2,
+    edge2: DVec2,
+}
 
 impl InsertHolder {
+    const INSERT_RADIUS: f64 = 2.0;
+    const WALL_THICKNESS: f64 = 2.0;
+    const HEIGHT: f64 = 7.0;
+    const RADIUS: f64 = Self::INSERT_RADIUS + Self::WALL_THICKNESS;
+
     pub fn from_outline_points(vertices: &[DVec2], index: usize, config: &Keyboard) -> Self {
-        const INSERT_RADIUS: f64 = 2.0;
-        const WALL_THICKNESS: f64 = 2.0;
-        const HEIGHT: f64 = 7.0;
+        let shell_thickness: f64 = config.shell_thickness.into();
+        let circumference_distance: f64 = config.circumference_distance.into();
 
         let n = vertices.len();
         let previous_vertex = vertices[(index + n - 1) % n];
         let vertex = vertices[index];
         let next_vertex = vertices[(index + 1) % n];
 
-        let edge1 = rotate_90_degrees(previous_vertex - vertex);
-        let edge2 = rotate_90_degrees(vertex - next_vertex);
+        let edge1 = rotate_90_degrees(previous_vertex - vertex).normalize();
+        let edge2 = rotate_90_degrees(vertex - next_vertex).normalize();
 
-        let outwards_direction = (edge1.normalize() + edge2.normalize()).normalize();
+        let outwards_direction = (edge1 + edge2).normalize();
         let point = vertex
-            + (f64::from(config.circumference_distance)
-                - INSERT_RADIUS
-                - WALL_THICKNESS.max(config.shell_thickness.into()))
+            + (circumference_distance
+                - Self::INSERT_RADIUS
+                - Self::WALL_THICKNESS.max(shell_thickness))
                 * outwards_direction;
 
-        let corner: Tree = Corner::new(edge1, edge2).into();
-        let hole: Tree = Circle::new(INSERT_RADIUS).into();
-        let rounded_corner = corner
-            .offset(INSERT_RADIUS + WALL_THICKNESS)
-            .difference(hole);
-        let tree = rounded_corner
-            .translate(dvec3(point.x, point.y, 0.0))
-            .extrude(0.0, HEIGHT);
-
-        Self(tree)
+        Self {
+            point,
+            edge1,
+            edge2,
+        }
     }
+
 }
 
 impl From<InsertHolder> for Tree {
     fn from(insert_holder: InsertHolder) -> Self {
-        insert_holder.0
+        let corner: Tree = Corner::new(insert_holder.edge1, insert_holder.edge2).into();
+        let hole: Tree = Circle::new(InsertHolder::INSERT_RADIUS).into();
+        let rounded_corner = corner.offset(InsertHolder::RADIUS).difference(hole);
+        rounded_corner
+            .translate(dvec3(insert_holder.point.x, insert_holder.point.y, 0.0))
+            .extrude(0.0, InsertHolder::HEIGHT)
     }
 }
