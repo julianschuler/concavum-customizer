16ad3007508d70261fd5267bc176daf40b4be59b
Julian Schuler <julianschuler@users.noreply.github.com>
2024-07-01 15:20:21 +0200

    Split model and mesh calculations

--- src/viewer/reload.rs
+++ src/viewer/reload.rs
@@ -16,19 +16,19 @@ use crate::{
     model::Model,
     viewer::{
         model::Mesh,
-        window::{ModelUpdater, ReloadEvent},
+        window::{SceneUpdate, SceneUpdater},
     },
 };
 
 pub struct ModelReloader {
-    updater: ModelUpdater,
+    updater: SceneUpdater,
     cancellation_token: CancellationToken,
     cache: Arc<Mutex<HashMap<Config, CpuMesh>>>,
 }
 
 impl ModelReloader {
     /// Creates a new model reloader using the given updater for updating the model.
-    pub fn new(updater: ModelUpdater) -> Self {
+    pub fn new(updater: SceneUpdater) -> Self {
         Self {
             updater,
             cancellation_token: CancellationToken::new(),
@@ -43,23 +43,25 @@ impl ModelReloader {
 
                 let model = Model::from_config(config.clone());
 
-                if let Some(keyboard) = self.cache.lock().unwrap().get(&config).cloned() {
-                    let model = model.with_keyboard(keyboard);
-                    self.updater.send_event(ReloadEvent::Finished(model));
+                self.updater
+                    .send_update(SceneUpdate::Model((&model).into()));
+
+                if let Some(mesh) = self.cache.lock().unwrap().get(&config).cloned() {
+                    self.updater.send_update(SceneUpdate::Mesh(mesh));
                 } else {
                     let cancellation_token = CancellationToken::new();
                     self.cancellation_token = cancellation_token.clone();
 
                     let start = Instant::now();
-                    self.updater.send_event(ReloadEvent::Started);
-                    let model = Model::from_config(config.clone());
 
-                    let mesh_settings = model.mesh_settings();
                     let cancellation_token = self.cancellation_token.clone();
                     let updater = self.updater.clone();
                     let cache = self.cache.clone();
 
                     spawn(move || {
+                        let mesh_settings = model.mesh_settings_preview();
+
+                        // Preview
                         let mut cancelled = false;
                         for depth in 0..mesh_settings.depth {
                             let mesh_settings = Settings {
@@ -67,22 +69,23 @@ impl ModelReloader {
                                 bounds: mesh_settings.bounds,
                                 threads: mesh_settings.threads,
                             };
-                            let model = model.mesh_preview(mesh_settings);
+                            let mesh = model.mesh_preview(mesh_settings);
 
                             cancelled = cancellation_token.cancelled();
                             if cancelled {
                                 break;
                             }
-                            updater.send_event(ReloadEvent::Updated(model));
+                            updater.send_update(SceneUpdate::Preview(mesh));
                         }
 
+                        // Final Mesh
                         if !cancelled {
-                            let model = model.mesh(mesh_settings);
+                            let mesh = model.mesh();
 
-                            cache.lock().unwrap().insert(config, model.keyboard.clone());
+                            cache.lock().unwrap().insert(config, mesh.clone());
 
                             if !cancellation_token.cancelled() {
-                                updater.send_event(ReloadEvent::Finished(model));
+                                updater.send_update(SceneUpdate::Mesh(mesh));
 
                                 eprintln!("Reloaded model in {:?}", start.elapsed());
                             }
@@ -90,7 +93,9 @@ impl ModelReloader {
                     });
                 }
             }
-            Err(error) => self.updater.send_event(ReloadEvent::Error(Arc::new(error))),
+            Err(error) => self
+                .updater
+                .send_update(SceneUpdate::Error(Arc::new(error))),
         }
     }
 }
