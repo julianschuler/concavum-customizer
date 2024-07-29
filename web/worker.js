ececd1170413da256dc125551f8d632880b8cc56
Julian Schuler <julianschuler@users.noreply.github.com>
2024-07-29 16:59:15 +0200

    Add static files required for running in a browser

---
+++ web/worker.js
@@ -0,0 +1,17 @@
+importScripts('./pkg/customizer.js')
+
+const { worker_entry_point } = wasm_bindgen;
+
+self.onmessage = async event => {
+  let init = await wasm_bindgen('./pkg/customizer_bg.wasm', event.data[0]).catch(err => {
+    setTimeout(() => {
+      throw err;
+    });
+    throw err;
+  });
+
+  worker_entry_point(event.data[1]);
+
+  init.__wbindgen_thread_destroy();
+  close();
+};
