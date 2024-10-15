540b9b4f7f46c6f25b3b12227ef2c81faec2dd19
Julian Schuler <julianschuler@users.noreply.github.com>
2024-10-15 16:42:31 +0200

    Add tolerance to interface PCB holder and cutouts

--- model/src/interface_pcb.rs
+++ model/src/interface_pcb.rs
@@ -19,14 +19,16 @@ pub struct InterfacePcb {
 impl InterfacePcb {
     const SIZE: DVec3 = dvec3(36.0, 42.0, 1.6);
     const HOLDER_THICKNESS: f64 = 1.0;
+    const TOLERANCE: f64 = 0.1;
 
     /// Creates a new interface PCB using the given insert holder.
     pub fn from_insert_holder(insert_holder: &InsertHolder) -> Self {
         let top_edge = insert_holder.outline_segment(Self::SIZE.x);
-        let translation = top_edge.start.extend(Self::HOLDER_THICKNESS);
-
         let direction = (top_edge.end - top_edge.start).normalize();
+
         let rotation_matrix = DMat2::from_cols(direction, rotate_90_degrees(direction));
+        let translation =
+            (top_edge.start + Self::TOLERANCE * direction).extend(Self::HOLDER_THICKNESS);
 
         let position =
             DAffine3::from_mat3_translation(DMat3::from_mat2(rotation_matrix), translation);
@@ -41,8 +43,12 @@ impl InterfacePcb {
         const BRACKET_WIDTH: f64 = 5.0;
         let height = Self::SIZE.z + Self::HOLDER_THICKNESS;
 
-        let cutout =
-            BoxShape::new(dvec3(Self::SIZE.x, bounds_diameter, bounds_diameter)).into_tree();
+        let cutout = BoxShape::new(dvec3(
+            Self::SIZE.x + 2.0 * Self::TOLERANCE,
+            bounds_diameter,
+            bounds_diameter,
+        ))
+        .into_tree();
         let cutout = cutout
             .translate(vec_z(
                 (bounds_diameter - height) / 2.0 + Self::HOLDER_THICKNESS,
@@ -55,22 +61,26 @@ impl InterfacePcb {
             height / 2.0 - Self::HOLDER_THICKNESS,
         );
 
-        BoxShape::new(dvec3(Self::SIZE.x + 2.0 * WIDTH, bounds_diameter, height))
-            .into_tree()
-            .difference(cutout)
-            .affine(self.position * DAffine3::from_translation(translation))
+        BoxShape::new(dvec3(
+            Self::SIZE.x + 2.0 * (WIDTH + Self::TOLERANCE),
+            bounds_diameter,
+            height,
+        ))
+        .into_tree()
+        .difference(cutout)
+        .affine(self.position * DAffine3::from_translation(translation))
     }
 
     /// Returns the cutouts required for the USB and TRRS ports for the given side.
     pub fn cutouts(&self, side: SideX, bounds_diameter: f64) -> Tree {
-        const USB_SIZE: DVec2 = dvec2(9.2, 3.4);
+        const USB_SIZE: DVec2 = dvec2(9.0, 3.2);
         const USB_RADIUS: f64 = 1.1;
         const USB_OFFSET: DVec3 = dvec3(24.9, 0.0, 3.2);
-        const JACK_RADIUS: f64 = 3.1;
+        const JACK_RADIUS: f64 = 3.0;
         const JACK_OFFSET_LEFT: DVec3 = dvec3(5.4, 0.0, 2.45);
         const JACK_OFFSET_RIGHT: DVec3 = dvec3(10.7, 0.0, 2.45);
 
-        let jack_cutout = Circle::new(JACK_RADIUS)
+        let jack_cutout = Circle::new(JACK_RADIUS + Self::TOLERANCE)
             .into_tree()
             .extrude(-Self::SIZE.y, bounds_diameter);
 
@@ -80,7 +90,7 @@ impl InterfacePcb {
         if matches!(side, SideX::Left) {
             let usb_cutout = Rectangle::new(USB_SIZE - DVec2::splat(2.0 * USB_RADIUS))
                 .into_tree()
-                .offset(USB_RADIUS)
+                .offset(USB_RADIUS + Self::TOLERANCE)
                 .extrude(-Self::SIZE.y, bounds_diameter);
 
             let usb_translation = DAffine3::from_translation(USB_OFFSET + height_offset);
