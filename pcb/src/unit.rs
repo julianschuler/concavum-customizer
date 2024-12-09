e10f211107100d17b620de7bbdfe087da75db080
Julian Schuler <julianschuler@users.noreply.github.com>
2024-12-09 17:06:01 +0100

    Implement arithmetic operations for primitives

--- pcb/src/unit.rs
+++ pcb/src/unit.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::ops::{Add, Mul, Neg, Sub};
 
 use serde::Serialize;
 
@@ -10,24 +10,73 @@ pub const VALUE_TO_UNIT: i32 = 1_000_000;
 #[serde(transparent)]
 pub struct Length(i32);
 
-impl Deref for Length {
-    type Target = i32;
+impl Add for Length {
+    type Output = Self;
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0)
+    }
+}
+
+impl Sub for Length {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0)
+    }
+}
+
+impl Mul<Length> for f32 {
+    type Output = Length;
+
+    fn mul(self, rhs: Length) -> Self::Output {
+        #[allow(clippy::cast_possible_truncation, clippy::cast_precision_loss)]
+        Length((self * rhs.0 as f32) as i32)
+    }
+}
+
+impl Neg for Length {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self(-self.0)
     }
 }
 
 /// A unit of rotation.
-#[derive(Serialize, Clone, Copy)]
+#[derive(Serialize, Clone, Copy, Default)]
 #[serde(transparent)]
 pub struct Angle(i32);
 
-impl Deref for Angle {
-    type Target = i32;
+impl Angle {
+    /// Calculates the sine and cosine of the angle.
+    pub fn sin_cos(self) -> (f32, f32) {
+        #[allow(clippy::cast_precision_loss)]
+        f32::sin_cos((self.0 as f32).to_radians())
+    }
+}
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Add for Angle {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self((self.0 + rhs.0) % (360 * VALUE_TO_UNIT))
+    }
+}
+
+impl Sub for Angle {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self((self.0 - rhs.0) % (360 * VALUE_TO_UNIT))
+    }
+}
+
+impl Neg for Angle {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self(-self.0)
     }
 }
 
@@ -63,12 +112,12 @@ impl IntoUnit for f32 {
 
 impl IntoUnit for f64 {
     fn mm(self) -> Length {
-        #[allow(clippy::cast_possible_truncation, clippy::cast_precision_loss)]
+        #[allow(clippy::cast_possible_truncation)]
         Length((self * f64::from(VALUE_TO_UNIT)) as i32)
     }
 
     fn deg(self) -> Angle {
-        #[allow(clippy::cast_possible_truncation, clippy::cast_precision_loss)]
+        #[allow(clippy::cast_possible_truncation)]
         Angle((self * f64::from(VALUE_TO_UNIT)) as i32)
     }
 }
