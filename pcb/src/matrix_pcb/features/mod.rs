2ddb616f70bac449e8ab1cab0a4eaf41d38651b0
Julian Schuler <julianschuler@users.noreply.github.com>
2024-12-12 15:06:22 +0100

    Use the connectors for calculating the features

--- pcb/src/matrix_pcb/features/mod.rs
+++ pcb/src/matrix_pcb/features/mod.rs
@@ -3,13 +3,13 @@ mod thumb_switches;
 
 use std::iter::once;
 
-use model::matrix_pcb::{
-    ClusterConnector, ColumnConnector, MatrixPcb as Model, NormalColumnConnector, Segment,
-    SideColumnConnector, CLUSTER_CONNECTOR_ARC_RADIUS, CONNECTOR_WIDTH, FPC_PAD_OFFSET,
-    FPC_PAD_SIZE, PAD_SIZE,
-};
+use model::matrix_pcb::{MatrixPcb as Model, FPC_PAD_OFFSET, FPC_PAD_SIZE};
 
-use crate::{position, primitives::Position, unit::IntoUnit};
+use crate::{
+    matrix_pcb::{connector::Connector, ORIGIN_POSITION},
+    position,
+    unit::IntoUnit,
+};
 
 pub use column::Column;
 pub use thumb_switches::ThumbSwitches;
@@ -18,6 +18,8 @@ pub use thumb_switches::ThumbSwitches;
 pub struct Features {
     pub columns: Vec<Column>,
     pub thumb_switches: ThumbSwitches,
+    pub column_connectors: Vec<Connector>,
+    pub cluster_connector: Connector,
 }
 
 impl Features {
@@ -27,63 +29,41 @@ impl Features {
         home_row_index: usize,
         cluster_connector_index: usize,
     ) -> Self {
-        let mut position = position!(100, 100, None);
+        let mut switch_position = ORIGIN_POSITION;
 
-        let columns: Vec<_> = once(position)
-            .chain(model.column_connectors.iter().map(|column_connector| {
-                position += match column_connector {
-                    ColumnConnector::Normal(NormalColumnConnector {
-                        bezier_curve,
-                        arc_radius,
-                        left_arc_side,
-                    }) => {
-                        let connector_length = left_arc_side.direction()
-                            * (2.0 * arc_radius + bezier_curve.length() - PAD_SIZE.y
-                                + CONNECTOR_WIDTH);
+        let column_connectors: Vec<_> = model
+            .column_connectors
+            .iter()
+            .map(|column_connector| {
+                let connector = Connector::from_column_connector(column_connector, switch_position);
+                switch_position = connector.end_switch_position();
+                connector
+            })
+            .collect();
 
-                        position!(2.0 * arc_radius + PAD_SIZE.x, connector_length, None)
-                    }
-                    ColumnConnector::Side(SideColumnConnector { connector, .. }) => {
-                        position!(connector.length() + PAD_SIZE.x, 0, None)
-                    }
-                };
-
-                position
-            }))
+        let columns: Vec<_> = once(ORIGIN_POSITION)
+            .chain(column_connectors.iter().map(Connector::end_switch_position))
             .zip(&model.column_key_connectors)
             .map(|(position, column_key_connectors)| {
                 Column::from_key_connectors(column_key_connectors, position, home_row_index)
             })
             .collect();
 
-        let anchor_position = columns[cluster_connector_index].first();
-        let first_thumb_switch =
-            first_thumb_switch_position(anchor_position, &model.cluster_connector);
-        let thumb_switches =
-            ThumbSwitches::from_key_connectors(&model.thumb_key_connectors, first_thumb_switch);
+        let cluster_connector_start = columns[cluster_connector_index].first()
+            + position!(0, FPC_PAD_OFFSET + FPC_PAD_SIZE.y / 2.0, Some(-90.deg()));
+        let cluster_connector =
+            Connector::from_cluster_connector(&model.cluster_connector, cluster_connector_start);
+
+        let thumb_switches = ThumbSwitches::from_key_connectors(
+            &model.thumb_key_connectors,
+            cluster_connector.end_switch_position(),
+        );
 
         Self {
             columns,
             thumb_switches,
+            column_connectors,
+            cluster_connector,
         }
     }
-}
-
-/// Calculates the position of the first thumb switch.
-fn first_thumb_switch_position(
-    anchor_position: Position,
-    cluster_connector: &ClusterConnector,
-) -> Position {
-    anchor_position
-        + position!(
-            -CLUSTER_CONNECTOR_ARC_RADIUS,
-            FPC_PAD_OFFSET + FPC_PAD_SIZE.y / 2.0,
-            Some(-cluster_connector.finger_cluster_arc_angle.rad())
-        )
-        + position!(
-            2.0 * CLUSTER_CONNECTOR_ARC_RADIUS,
-            cluster_connector.bezier_curve.length(),
-            Some(cluster_connector.thumb_cluster_arc_angle.rad())
-        )
-        + position!(-CLUSTER_CONNECTOR_ARC_RADIUS, PAD_SIZE.y / 2.0, None)
 }
