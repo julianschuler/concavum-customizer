ffba1a8b09bbae05acfb9f8bc985f6f667e25248
Julian Schuler <julianschuler@users.noreply.github.com>
2024-12-14 19:33:18 +0100

    Implement adding/subtracting a Position and Point

--- pcb/src/primitives.rs
+++ pcb/src/primitives.rs
@@ -13,6 +13,24 @@ impl Point {
     pub const fn new(x: Length, y: Length) -> Self {
         Self(x, y)
     }
+
+    /// Returns the X coordinate of the position.
+    pub fn x(self) -> Length {
+        self.0
+    }
+
+    /// Returns the Y coordinate of the position.
+    pub fn y(self) -> Length {
+        self.1
+    }
+}
+
+impl Neg for Point {
+    type Output = Point;
+
+    fn neg(self) -> Self::Output {
+        Self(-self.0, -self.1)
+    }
 }
 
 /// Creates a point from the given coordinates in millimeter.
@@ -78,10 +96,8 @@ impl Position {
         Point::new(self.x(), self.y())
     }
 
-    /// Applies the affine transform given by the other position to `self`.
-    ///
-    /// The transform is applied in the reference frame of `self`.
-    pub fn affine(self, other: Position) -> Self {
+    /// Applies the affine transform given by `self` to the given position.
+    pub fn transform_position(self, other: Position) -> Self {
         let (sin, cos) = self.angle().unwrap_or_default().sin_cos();
 
         let x = self.x() + cos * other.x() + sin * other.y();
@@ -95,13 +111,23 @@ impl Position {
 
         Self::new(x, y, angle)
     }
+
+    /// Applies the affine transform given by `self` the the given point.
+    pub fn transform_point(self, point: Point) -> Point {
+        let (sin, cos) = self.angle().unwrap_or_default().sin_cos();
+
+        let x = self.x() + cos * point.x() + sin * point.y();
+        let y = self.y() - sin * point.x() + cos * point.y();
+
+        Point::new(x, y)
+    }
 }
 
 impl Add for Position {
     type Output = Position;
 
     fn add(self, rhs: Self) -> Self::Output {
-        self.affine(rhs)
+        self.transform_position(rhs)
     }
 }
 
@@ -109,7 +135,23 @@ impl Sub for Position {
     type Output = Position;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        self.affine(-rhs)
+        self.transform_position(-rhs)
+    }
+}
+
+impl Add<Point> for Position {
+    type Output = Point;
+
+    fn add(self, rhs: Point) -> Self::Output {
+        self.transform_point(rhs)
+    }
+}
+
+impl Sub<Point> for Position {
+    type Output = Point;
+
+    fn sub(self, rhs: Point) -> Self::Output {
+        self.transform_point(-rhs)
     }
 }
 
