eb5046dd0e56172b448085faac7c872884b946db
Julian Schuler <julianschuler@users.noreply.github.com>
2024-12-22 19:56:00 +0100

    Extract FFC pad position calculation

--- pcb/src/matrix_pcb/features/ffc_connector.rs
+++ pcb/src/matrix_pcb/features/ffc_connector.rs
@@ -1,16 +1,15 @@
 use model::matrix_pcb::{CONNECTOR_WIDTH, FFC_PAD_OFFSET, FFC_PAD_SIZE, PAD_SIZE};
 
 use crate::{
+    footprints::ROW_PAD,
     kicad_pcb::{KicadPcb, Net},
     matrix_pcb::{
-        centered_track_offset,
-        nets::{self, Nets},
-        AddPath, BOTTOM_LAYER, TOP_LAYER,
+        centered_track_offset, features::Column, nets::Nets, AddPath, BOTTOM_LAYER, TOP_LAYER,
     },
     path::Path,
     point, position,
     primitives::Position,
-    unit::IntoAngle,
+    unit::{IntoAngle, Length},
 };
 
 /// An FFC connector.
@@ -19,8 +18,9 @@ pub struct FfcConnector {
 }
 
 impl FfcConnector {
-    const OFFSET: f32 = 5.5;
-    const PITCH: f32 = 1.0;
+    const Y_OFFSET: Length = Length::new(5.5);
+    const PITCH: Length = Length::new(1.0);
+    const PAD_OFFSET: Length = Length::new(0.6);
 
     /// Creates a new FFC connector from the given anchor switch position.
     pub fn from_anchor(anchor: Position) -> Self {
@@ -34,20 +34,20 @@ impl FfcConnector {
 
     /// Returns the position of the FFC connector.
     pub fn position(&self) -> Position {
-        self.anchor + position!(0, Self::OFFSET, None)
+        self.anchor + position!(0, Self::Y_OFFSET, None)
     }
 
     /// Adds the outline of the FFC connector to the PCB.
     pub fn add_outline(&self, pcb: &mut KicadPcb) {
         for sign in [-1.0, 1.0] {
-            let ffc_pad_top_offset = FFC_PAD_OFFSET - FFC_PAD_SIZE.y / 2.0;
-            let ffc_pad_bottom_offset = FFC_PAD_OFFSET + FFC_PAD_SIZE.y / 2.0;
+            let pad_top_offset = FFC_PAD_OFFSET - FFC_PAD_SIZE.y / 2.0;
+            let pad_bottom_offset = FFC_PAD_OFFSET + FFC_PAD_SIZE.y / 2.0;
 
             let outline_points = [
-                self.anchor + point!(sign * CONNECTOR_WIDTH / 2.0, ffc_pad_bottom_offset),
-                self.anchor + point!(sign * FFC_PAD_SIZE.x / 2.0, ffc_pad_bottom_offset),
-                self.anchor + point!(sign * FFC_PAD_SIZE.x / 2.0, ffc_pad_top_offset),
-                self.anchor + point!(sign * PAD_SIZE.x / 2.0, ffc_pad_top_offset),
+                self.anchor + point!(sign * CONNECTOR_WIDTH / 2.0, pad_bottom_offset),
+                self.anchor + point!(sign * FFC_PAD_SIZE.x / 2.0, pad_bottom_offset),
+                self.anchor + point!(sign * FFC_PAD_SIZE.x / 2.0, pad_top_offset),
+                self.anchor + point!(sign * PAD_SIZE.x / 2.0, pad_top_offset),
                 self.anchor + point!(sign * PAD_SIZE.x / 2.0, -PAD_SIZE.y / 2.0),
             ];
 
@@ -74,17 +74,15 @@ impl FfcConnector {
         nets: &Nets,
         thumb_switch_count: usize,
     ) {
-        const PAD_OFFSET: f32 = 0.6;
-
-        let ffc_pad_bottom_offset = FFC_PAD_OFFSET + FFC_PAD_SIZE.y / 2.0;
+        let pad_bottom_offset = FFC_PAD_OFFSET + FFC_PAD_SIZE.y / 2.0;
         let row_net = &nets.rows[0];
         let (first_column_net, column_nets) = nets.columns[..thumb_switch_count]
             .split_first()
             .expect("there is at least one thumb switch");
 
         let row_path = Path::angled_center(
-            point!(0, ffc_pad_bottom_offset),
-            point!(5.5 * Self::PITCH, Self::OFFSET + PAD_OFFSET),
+            point!(0, pad_bottom_offset),
+            point!(Self::pad_x_offset(11), Self::Y_OFFSET + Self::PAD_OFFSET),
         )
         .at(self.anchor);
         pcb.add_track(&row_path, TOP_LAYER, row_net);
@@ -94,23 +92,27 @@ impl FfcConnector {
                 centered_track_offset(0, thumb_switch_count),
                 pad_bottom_offset
             ),
-            point!(-5.5 * Self::PITCH, Self::OFFSET + PAD_OFFSET),
+            point!(Self::pad_x_offset(0), Self::Y_OFFSET + Self::PAD_OFFSET),
         )
         .at(self.anchor);
         pcb.add_track(&first_column_path, BOTTOM_LAYER, first_column_net);
 
         for (i, column_net) in column_nets.iter().enumerate() {
-            #[allow(clippy::cast_precision_loss)]
-            let ffc_connector_x_offset = (i as f32 + 0.5) * Self::PITCH;
             let path = Path::angled_start(
                 point!(
                     centered_track_offset(i + 1, thumb_switch_count),
                     pad_bottom_offset
                 ),
-                point!(ffc_connector_x_offset, Self::OFFSET + PAD_OFFSET),
+                point!(Self::pad_x_offset(i + 6), Self::Y_OFFSET + Self::PAD_OFFSET),
             )
             .at(self.anchor);
             pcb.add_track(&path, BOTTOM_LAYER, column_net);
         }
+    }
+
+    /// Returns the offset in the X direction of the connector pad with the given index.
+    #[allow(clippy::cast_precision_loss)]
+    fn pad_x_offset(index: usize) -> Length {
+        (index as f32 - 5.5) * Self::PITCH
     }
 }
